985b140fa4788ab7a7a58b5b0c584743ad15a57e
Tiffany White <hey@tiffanyrwhite.com>
2020-06-20 03:12:36 -0400

    Update something

--- sw.js
+++ sw.js
@@ -21,9 +21,9 @@ const RUNTIME = 'runtime';
 const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
-  'styles.css',
-  '../../styles/main.css',
-  'demo.js'
+  'resume.min.css',
+  '../../css/resume.min.css',
+  'js/resume.min.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
